38fd4a99294f404fe62a06edb7bfd6c0ad09faef
OpenSense Labs MBP - 1 <osl_mbp-1@OpenSenses-MacBook-Pro.local>
2019-06-27 20:14:37 +0530

    27th-june

---
+++ js/bauhau.js
@@ -0,0 +1,10 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(51.508742,-0.120850),
+  zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
